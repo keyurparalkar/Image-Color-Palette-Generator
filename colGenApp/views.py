faa1da13a0630a0db6a747ff213db05113d5f825
keyurparalkar <keur.plkar@gmail.com>
2020-08-30 17:18:42 +0530

    Added Form, template and view

--- colGenApp/views.py
+++ colGenApp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .forms import ImageUploadForm
 
 # Create your views here.
 def root_view(request):
-    return render(request, 'colGenApp/index.html',{})
+    print(f"REQUEST METHOD TYPE ========= {request.method}")
+    form = ImageUploadForm(request.POST, request.FILES)
+    if request.method == "POST":
+        if form.is_valid():
+            print("IMAGE NAME === ", form.cleaned_data.get('name'))
+            return render(request, 'colGenApp/index.html',{'form':form})
+    else:
+        form = ImageUploadForm()
+        return render(request, 'colGenApp/index.html',{'form':form})
