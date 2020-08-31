caead1f1692b4e6a0bb1ba636900ace7fb1748f4
keyurparalkar <keur.plkar@gmail.com>
2020-08-31 09:32:36 +0530

    Displaying image on homepage with django model

--- colGenApp/views.py
+++ colGenApp/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from .forms import ImageUploadForm
+import base64
 
 # Create your views here.
 def root_view(request):
-    print(f"REQUEST METHOD TYPE ========= {request.method}")
-    form = ImageUploadForm(request.POST, request.FILES)
     if request.method == "POST":
+        form = ImageUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            print("IMAGE NAME === ", form.cleaned_data.get('name'))
-            print("POST SUCCESS")
-            return render(request, 'colGenApp/index.html',{'form':form})
+            img_obj = request.FILES['img_file'].read()
+            img_obj64_bytes = base64.b64encode(img_obj)
+            img_obj64_decoded = img_obj64_bytes.decode('ascii')
+            return render(request, 'colGenApp/index.html',{'form':form, 'img_obj':img_obj64_decoded})
     else:
         form = ImageUploadForm()
     
