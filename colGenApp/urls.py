a6496d8d03030cb2a163571c3d8235d47500c39e
keyurparalkar <keur.plkar@gmail.com>
2020-09-03 09:12:48 +0530

    Created a basic viewset

--- colGenApp/urls.py
+++ colGenApp/urls.py
@@ -1,5 +1,18 @@
-from django.urls import path
-from . import views 
+from django.urls import path, include
+from rest_framework import routers, serializers, viewsets
+
+from . import views
+from .serializers import ImageSerializer
+from .models import ImageUploadModel
+
+#creating viewsets to define view behaviour
+class ImageViewSet(viewsets.ModelViewSet):
+    queryset = ImageUploadModel.objects.all()
+    serializer_class = ImageSerializer
+
+router = routers.DefaultRouter()
+router.register(r'all_imgs', ImageViewSet)
+
 
 urlpatterns = [
     path('', views.root_view, name="root_view")
