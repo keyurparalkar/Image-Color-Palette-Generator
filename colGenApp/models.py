6eb701d533dacec12273962a1af04509bce67c29
keyurparalkar <keur.plkar@gmail.com>
2020-09-22 08:23:03 +0530

    Models, View, serializer enhancements

--- colGenApp/models.py
+++ colGenApp/models.py
@@ -1,5 +1,13 @@
 from django.db import models
-
+from django.core.validators import validate_image_file_extension
 # Create your models here.
 class ImageUploadModel(models.Model):
-    img = models.ImageField(upload_to='')
+    created = models.DateTimeField(auto_now_add=True)
+    img = models.ImageField(upload_to='', validators=[validate_image_file_extension])
+
+    # id = models.IntegerField(primary_key=True)
+    # text_code = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ['-created']
+    
--- colGenApp/urls.py
+++ colGenApp/urls.py
@@ -2,18 +2,10 @@ from django.urls import path, include
 from rest_framework import routers, serializers, viewsets
 
 from . import views
-from .serializers import ImageSerializer
 from .models import ImageUploadModel
-
-#creating viewsets to define view behaviour
-class ImageViewSet(viewsets.ModelViewSet):
-    queryset = ImageUploadModel.objects.all()
-    serializer_class = ImageSerializer
-
-router = routers.DefaultRouter()
-router.register(r'all_imgs', ImageViewSet)
 
 
 urlpatterns = [
-    path('', views.root_view, name="root_view")
+    path('', views.root_view, name="root_view"),
+    path('data_list/',views.data_list, name='data_list')
 ]
